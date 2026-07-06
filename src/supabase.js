1d7e1a153159884f1ac188d1855d3f6ab94aad57
talealiu-sketch <tale.aliu@inkodeks.com>
2026-07-06 16:59:20 +0200

    fix supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mixlrpflbfhjdasteieu.supabase.co/rest/v1/';
+const supabaseUrl = 'https://abcdefghijk.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1peGxycGZsYmZoamRhc3RlaWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNDM3MjEsImV4cCI6MjA5ODkxOTcyMX0.ytUQ6FQQFahHKHdeq52XZQ4BeV92mS7Eomlera90T8o';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
