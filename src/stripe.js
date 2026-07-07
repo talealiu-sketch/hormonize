93faf17158f8a36a8a7e348b4055108a3f4aa771
talealiu-sketch <tale.aliu@inkodeks.com>
2026-07-07 15:07:16 +0200

    fix stripe price id

--- src/stripe.js
+++ src/stripe.js
@@ -5,7 +5,6 @@ const stripePublishableKey = 'pk_test_51TqYCBIVLKqOkL4KGq1Nt88ojSBl7EK8DmYi1se08
 export const stripe = loadStripe(stripePublishableKey);
 
 export const PRICES = {
-  basic_monthly: 'price_1TqYJKIVLKqOkL4KyJ9wPfec
-',
+  basic_monthly: 'price_1TqYJKIVLKqOkL4KyJ9wPfec',
   pro_monthly: 'price_1TqYKMIVLKqOkL4KnwzA9Vbz',
 };
